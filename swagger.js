3e3f09b2aaebcbdeeff59472a52aeb2ca828d849
ToKolob <anolis27@hotmail.com>
2025-01-18 18:34:12 -0300

    ok

--- swagger.js
+++ swagger.js
@@ -6,13 +6,10 @@ const doc = {
     description: 'Contacts API for CSE341 project',
   },
   host: 'https://cse341-project-contacts.onrender.com',
-  schemes: ['http', 'https'],
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const routes = ['./routes/index.js'];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen(outputFile, routes, doc);
